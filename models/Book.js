91efddc8724e1e5c6f8354d234f3cd332cb0b541
Shehab <shehab200088111@gmail.com>
2025-10-04 17:54:30 +0300

    Added the helper file inside project_guide folder to maintain structure before merge and begain the testing process (Shehab and Nada)

--- models/Book.js
+++ models/Book.js
@@ -2,36 +2,44 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const bookSchema = new schema({
-    title:{
-        type:String,
-        required:true
-    },
-     author:{
-        type:String,
-        required:true
-    },
-     price:{
-        type:Number,
-        required:true
-    },
-     description:{
-        type:String,
-        required:true
-    },
-     stock:{
-        type:Number,
-        
-        min:0,
-        default:0
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  stock: {
+    type: Number,
 
+    min: 0,
+    default: 0,
+  },
+  image: {
+    type: String,
+  },
+  pdf: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
     },
-    image:{
-        type:String
-    },
-    reviews:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Review"
-    }]
-})
+  ],
+});
 
 module.exports = mongoose.model("Book", bookSchema);
