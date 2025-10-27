06e307719d899aacde9b8d2e8b963cb60b3f3ed1
Shehab <shehab200088111@gmail.com>
2025-10-28 01:43:14 +0300

    fixed orders and payment

--- models/Order.js
+++ models/Order.js
@@ -1,33 +1,44 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const orderSchema = new schema({
-  User: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  Books: [
-    {
-      BookId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Book",
-      },
-      Quantity: {
-        type: Number,
-        default: 1,
-      },
+const orderSchema = new schema(
+  {
+    User: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-  ],
-
-  TotalPrice: {
-    type: Number,
-    required: true,
+    Books: [
+      {
+        BookId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Book",
+          required: true,
+        },
+        Quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    TotalPrice: {
+      type: Number,
+      required: true,
+    },
+    Status: {
+      type: String,
+      enum: ["pending", "completed"],
+      default: "pending",
+    },
+    PaymentIntentId: {
+      type: String,
+      required: true,
+    },
   },
-  Status: {
-    type: String,
-    enum: ["pending", "completed"],
-    default: "pending",
-  },
-});
+  {
+    timestamps: { createdAt: "CreatedAt", updatedAt: false },
+  }
+);
 
 module.exports = mongoose.model("Order", orderSchema);
